61d70171deb225dcf4467f0c9a59d5b8106b864f
haskog <hakon.skogsto@gmail.com>
2019-06-25 12:54:50 +0200

    styles elements and adds tech section on about page

---
+++ views/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :style="{ backgroundColor: activeColor }">
+        <h1>Page 3 - Change background color</h1>
+        <p>Enter a color and watch the magic!</p>
+        <input type="text" v-model="activeColor">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeColor: "red"
+        }
+    }
+}
+</script>
